60e49b3ae564a4d67a581e6cb87f52345bd376fb
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2023-07-22 23:44:13 +0300

    Add notifications

--- Mobile/selfdiary/lib/main.dart
+++ Mobile/selfdiary/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +19,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseMessaging.instance.requestPermission();
   setupLocator();
   await ensureLoggedIn();
   runApp(const MyApp());
